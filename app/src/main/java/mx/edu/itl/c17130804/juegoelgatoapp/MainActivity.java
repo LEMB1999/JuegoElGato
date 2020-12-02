d7d72a4a015f9f87866de03a0f74ce87385f7db1
KillyRex2 <55725996+KillyRex2@users.noreply.github.com>
2020-12-01 20:58:29 -0600

    Primer Comit

--- app/src/main/java/mx/edu/itl/c17130804/juegoelgatoapp/MainActivity.java
+++ app/src/main/java/mx/edu/itl/c17130804/juegoelgatoapp/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String lui = "Peñejillo"; 
+
     }
 
 }
