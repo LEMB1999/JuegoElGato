58c3e8afaa73c4e9e0781b76839ddc0c572e2e0f
Luis Emmanuel Mendez Barrios <mendezemmanuel1999@gmail.com>
2020-12-03 17:18:59 -0600

    Version 1.0

--- app/src/main/java/mx/edu/itl/c17130804/juegoelgatoapp/MainActivity.java
+++ app/src/main/java/mx/edu/itl/c17130804/juegoelgatoapp/MainActivity.java
@@ -1,56 +1,59 @@
 package mx.edu.itl.c17130804.juegoelgatoapp;
 
-import android.content.Context;
-import android.media.Image;
+
 import android.os.Bundle;
-
 import com.bumptech.glide.Glide;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
-import android.widget.GridLayout;
-import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public class MainActivity extends AppCompatActivity {
 
 
-
+    TextView text;
     ArrayList<ImageView> arr ;
+    int  ids[];
+    ArrayList<Integer>  arr_valores ;
     int turno = 0;
-
+    int  num_turnos = 0;
+    int coordenadas [][] = { {0,1,2},{0,3,6},{2,5,8},{6,7,8},{0,4,8},{2,4,6}};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //inicializar atributos
         arr = new ArrayList<>(9);
+        text = findViewById(R.id.txtJugador);
+        arr_valores = new ArrayList<Integer>( Arrays.asList(-1,-1,-1,-1,-1,-1,-1,-1,-1));
+        ids = new int[9];
 
-        arr.add(findViewById(R.id.img1));
-        arr.add(findViewById(R.id.img2));
-        arr.add(findViewById(R.id.img3));
-        arr.add(findViewById(R.id.img4));
-        arr.add(findViewById(R.id.img5));
-        arr.add(findViewById(R.id.img6));
-        arr.add(findViewById(R.id.img7));
-        arr.add(findViewById(R.id.img8));
-        arr.add(findViewById(R.id.img9));
+        ids[0] = R.id.img1;
+        ids[1] = R.id.img2;
+        ids[2] = R.id.img3;
+        ids[3] = R.id.img4;
+        ids[4] = R.id.img5;
+        ids[5] = R.id.img6;
+        ids[6] = R.id.img7;
+        ids[7] = R.id.img8;
+        ids[8] = R.id.img9;
+
+        arr.add(findViewById(ids[0]));
+        arr.add(findViewById(ids[1]));
+        arr.add(findViewById(ids[2]));
+        arr.add(findViewById(ids[3]));
+        arr.add(findViewById(ids[4]));
+        arr.add(findViewById(ids[5]));
+        arr.add(findViewById(ids[6]));
+        arr.add(findViewById(ids[7]));
+        arr.add(findViewById(ids[8]));
 
     }
 
@@ -67,11 +70,81 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void cambiar_imagen(View view){
-          turno = turno == 0?1:0;
+
+        if( turno == 0){
+            text.setText("jugador 2");
+        }else{
+            text.setText("jugador 1");
+        }
+
+          actualiza_arreglo(view.getId(),turno);
           cambiar_imagen(view.getId(),turno);
+          turno = turno == 0?1:0;
+          boolean flag1 = false,flag2 = false;
+          if (num_turnos>=4){
+              flag1 = verificar_ganador(0);
+              flag2 = verificar_ganador(1);
+          }else{
+              num_turnos++;
+          }
+          //desactivar boton
+          view.setEnabled(false);
+
+          if(flag1){
+              Toast.makeText(this,"Gano el jugador 1",Toast.LENGTH_LONG).show();
+              desactivar_botones();
+          }else if(flag2){
+              Toast.makeText(this,"Gano el jugador 2",Toast.LENGTH_LONG).show();
+              desactivar_botones();
+          }
+
+    }
+
+    public void actualiza_arreglo(int id,int turno){
+         if( id == ids[0] ){
+             arr_valores.set(0,turno);
+         }else if( id == ids[1] ){
+             arr_valores.set(1,turno);
+         }else if( id == ids[2] ){
+             arr_valores.set(2,turno);
+         }else if( id == ids[3] ){
+             arr_valores.set(3,turno);
+         }else if( id == ids[4] ){
+             arr_valores.set(4,turno);
+         }else if( id == ids[5] ){
+             arr_valores.set(5,turno);
+         }else if( id == ids[6] ){
+             arr_valores.set(6,turno);
+         }else if( id == ids[7] ){
+             arr_valores.set(7,turno);
+         }else if( id == ids[8] ){
+             arr_valores.set(8,turno);
+         }
     }
 
 
+    public boolean verificar_ganador(int jugador){
+        Log.d("datos",arr_valores.toString());
+        int acomulador;
+        for (int i = 0; i<6; i++){
+            acomulador = 0;
+            for (int j = 0; j < 3; j++){
+                 if( jugador == arr_valores.get(coordenadas[i][j]) ){
+                     acomulador++;
+                 }
+            }
+            if(acomulador == 3){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void desactivar_botones(){
+            for( int i = 0; i<arr.size();i++){
+                arr.get(i).setEnabled(false);
+            }
+    }
 
 
 
