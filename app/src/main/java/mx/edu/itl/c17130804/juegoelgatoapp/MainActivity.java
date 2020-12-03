ed3716a7c1996397276a81efba8b46ec7ce33629
Luis Emmanuel Mendez Barrios <mendezemmanuel1999@gmail.com>
2020-12-03 00:36:34 -0600

    diseño terminado

--- app/src/main/java/mx/edu/itl/c17130804/juegoelgatoapp/MainActivity.java
+++ app/src/main/java/mx/edu/itl/c17130804/juegoelgatoapp/MainActivity.java
@@ -1,27 +1,79 @@
 package mx.edu.itl.c17130804.juegoelgatoapp;
 
+import android.content.Context;
+import android.media.Image;
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    ArrayList<ImageView> arr ;
+    int turno = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        arr = new ArrayList<>(9);
+
+        arr.add(findViewById(R.id.img1));
+        arr.add(findViewById(R.id.img2));
+        arr.add(findViewById(R.id.img3));
+        arr.add(findViewById(R.id.img4));
+        arr.add(findViewById(R.id.img5));
+        arr.add(findViewById(R.id.img6));
+        arr.add(findViewById(R.id.img7));
+        arr.add(findViewById(R.id.img8));
+        arr.add(findViewById(R.id.img9));
 
     }
+
+
+
+    public void  cambiar_imagen(int id, int turno){
+        ImageView img = findViewById(id);
+        if( turno == 0){
+            Glide.with(this).load(R.drawable.tacha).into(img);
+        }else{
+            Glide.with(this).load(R.drawable.circulo).into(img);
+        }
+    }
+
+
+    public void cambiar_imagen(View view){
+          turno = turno == 0?1:0;
+          cambiar_imagen(view.getId(),turno);
+    }
+
+
+
+
 
 
 }
--- app/src/main/java/mx/edu/itl/c17130804/juegoelgatoapp/Menu.java
+++ app/src/main/java/mx/edu/itl/c17130804/juegoelgatoapp/Menu.java
@@ -2,11 +2,16 @@ package mx.edu.itl.c17130804.juegoelgatoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
+
+    Button  btnPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +19,14 @@ public class Menu extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_menu);
+        btnPlay = findViewById(R.id.btnPlay);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
